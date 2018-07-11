ef8409229b7f62992159bd7e4709a35500e285e3
Rok <roksetnik123@gmail.com>
2018-07-11 12:57:02 +0200

    mDiff modify

--- src/at/aau/MDiff.java
+++ src/at/aau/MDiff.java
@@ -1,12 +1,33 @@
 package at.aau;
 
 public class MDiff {
-    private final long id;
-    private final String content;
+    
+    private final int private String actionType;
+    private final int srcID;
+    private final int srcStartLine;
+    private final int srcStartLineOffset;
+    private final int srcEndLine;
+    private final int srcEndLineOffset;
 
-    public MDiff(long id, String content) {
-        this.id =id;
-        this.content =content;
+    private final int dstID;
+    private final int dstStartLine;
+    private final int dstStartLineOffset;
+    private final int dstEndLine;
+    private final int dstEndLineOffset;
+
+    public MDiff(String actionType, int srcID, int srcStartLine, int srcStartLineOffset, int srcEndLine, int srcEndLineOffset, int dstID, int dstStartLine, int dstStartLineOffset, int dstEndLine, int dstEndLineOffset) {
+        this.actionType =actionType;
+        this.srcID =srcID;
+        this.srcStartLine =srcStartLine;
+        this.srcStartLineOffset =srcStartLineOffset;
+        this.srcEndLine =srcEndLine;
+        this.srcEndLineOffset =srcEndLineOffset;
+
+        this.dstID =dstID;
+        this.dstStartLine =dstStartLine;
+        this.dstStartLineOffset =dstStartLineOffset;
+        this.dstEndLine =dstEndLine;
+        this.dstEndLineOffset =dstEndLineOffset;
     }
 
     public long getId() {
