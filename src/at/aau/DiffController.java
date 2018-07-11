4fa6b6121ecfedb163cfc4b570a21d350caaf96a
Friedrich <friedrich-gi@outlook.com>
2018-07-11 12:36:06 +0200

    test

--- src/at/aau/DiffController.java
+++ src/at/aau/DiffController.java
@@ -1,14 +1,27 @@
 package at.aau;
 
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+
+import static at.aau.DiffMain.diffFiles;
 
 @RestController
 public class DiffController {
 
+    List<DiffInfo> myList = diffFiles("originalFile.txt", "revisedFile.txt");
+    private String template = myList.get(0).getActionType();
+    private final AtomicLong counter = new AtomicLong();
+
     @RequestMapping("/mdiff")
-    public String index() {
-        return "Do something useful here!";
+
+
+
+    public MDiff mdiff() {
+        return new MDiff(counter.incrementAndGet(), String.format(template));
     }
 
 }
---
+++ src/at/aau/DiffMain.java
@@ -0,0 +1,108 @@
+package at.aau;
+
+import at.aau.DiffInfo;
+import difflib.Delta;
+import difflib.DiffUtils;
+import difflib.Patch;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class DiffMain {
+    public static List<DiffInfo> diffFiles(String srcFilename, String dstFilename) {
+
+        List<String> original = fileToLines(srcFilename);
+        List<String> revised  = fileToLines(dstFilename);
+
+        // Compute diff. Get the Patch object. Patch is the container for computed deltas.
+        Patch patch = DiffUtils.diff(original, revised);
+
+
+        ArrayList<DiffInfo> diffList = new ArrayList<>();
+
+        int i = 0;
+
+
+        for (Object o : patch.getDeltas()) {
+            DiffInfo diffInfo = new DiffInfo();
+            Delta delta = (Delta) o;
+            System.out.println(delta.toString());
+
+
+
+            /*int u = delta.getOriginal().toString().substring(2).indexOf("[") + 3; // start quo. ori.
+            int v = delta.getOriginal().toString().length() - 2; // end quo. ori.
+            diffInfo.setSrcEndLineOffset(delta.getOriginal().toString().substring(u,v).length());*/
+            diffInfo.setSrcEndLineOffset((original.get(original.size()-1).length()));
+
+            /*int s = delta.getRevised().toString().substring(2).indexOf("[") + 3; //start quotation revised content
+            int t = delta.getRevised().toString().length() - 2; //end quotation revised content
+            diffInfo.setDstEndLineOffset(delta.getRevised().toString().substring(s,t).length()); */
+            diffInfo.setDstEndLineOffset((revised.get(revised.size()-1).length()));
+
+
+            if (delta.getType().equals(Delta.TYPE.CHANGE)) {
+                diffInfo.setActionType("UPDATE");
+            } else if (delta.getType().equals(Delta.TYPE.INSERT)) {
+                diffInfo.setActionType("INSERT");
+            } else if (delta.getType().equals(Delta.TYPE.DELETE)) {
+                diffInfo.setActionType("DELETE");
+            } else {
+                diffInfo.setActionType("UNKNOWN");
+            }
+
+
+            diffInfo.setSrcID(i);
+            diffInfo.setSrcStartLine(delta.getOriginal().getPosition());
+            diffInfo.setSrcEndLine(delta.getOriginal().size() - 1 + delta.getOriginal().getPosition());
+
+
+            diffInfo.setDstID(i);
+            diffInfo.setDstStartLine(delta.getRevised().getPosition());
+
+            if (diffInfo.getActionType().equals("DELETE")) {
+                diffInfo.setDstEndLine(delta.getRevised().size() + delta.getRevised().getPosition());
+            } else {
+                diffInfo.setDstEndLine(delta.getRevised().size() - 1 + delta.getRevised().getPosition());
+            }
+
+
+
+            diffList.add(diffInfo);
+
+            System.out.println("ActionType: " + diffInfo.getActionType());
+
+            System.out.println("srcID: " + diffInfo.getSrcID());
+            System.out.println("srcStartLine: " + diffInfo.getSrcStartLine());
+            System.out.println("srcEndline: " + diffInfo.getSrcEndLine());
+            System.out.println("srcEndlineOffset: " + diffInfo.getSrcEndLineOffset());
+
+            System.out.println("dstStartLine: " + diffInfo.getDstStartLine());
+            System.out.println("dstEndline: " + diffInfo.getDstEndLine());
+            System.out.println("dstEndlineOffset: " + diffInfo.getDstEndLineOffset());
+            System.out.println("dstID: " + diffInfo.getDstID());
+
+            i++;
+        }
+        return diffList;
+    }
+
+    public static List<String> fileToLines(String fileName) {
+        ArrayList<String> liste = new ArrayList<>();
+
+        Path path = Paths.get(fileName);
+        try (Stream<String> lines = Files.lines(path)) {
+            lines.forEach(liste::add);
+        }  catch (IOException ex) {
+            System.out.println("Something doesn't work!");
+        }
+        return liste;
+    }
+
+
+}
