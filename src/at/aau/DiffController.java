5f72cc510b3a01e8a23208eba6b753b9df7ac979
Friedrich <friedrich-gi@outlook.com>
2018-07-12 13:27:33 +0200

    DiffController updated

--- src/at/aau/DiffController.java
+++ src/at/aau/DiffController.java
@@ -1,6 +1,7 @@
 package at.aau;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -8,10 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class DiffController {
 
     @RequestMapping("/mdiff")
-    public String mdiff() {
-        return new MDiff("Textfiles/originalFile.txt", "TextFiles/revisedFile.txt").getReturnString();
+    public MDiff mdiff(@RequestParam(value="srcID", defaultValue="0") int srcID) {
+        return new MDiff(srcID,"Textfiles/originalFile.txt", "TextFiles/revisedFile.txt");  //.getReturnString()
 //        new MDiff("Textfiles/originalFile.txt", "TextFiles/revisedFile.txt");
-//        return "test";
+//        return "test"
     }
-
 }
