f875519e34a3d1144369701b1f918a389aa4a717
Friedrich <friedrich-gi@outlook.com>
2018-07-30 10:06:12 +0200

    added new Line "sechs" in revisedFile.txt
    started with new approach to get data from Algorithm
        - not comparing the lines with the same number from both files, but diff the Files, and then calculate DiffInfo values

--- src/at/aau/DiffMatchPatch/DmpMain.java
+++ src/at/aau/DiffMatchPatch/DmpMain.java
@@ -7,34 +7,52 @@ import org.apache.commons.io.IOUtils;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class DmpMain {
 
-    private String srcText;
-    private String dstText;
+    private List<String> srcList;
+    private List<String> dstList;
 
+    public static List<String> fileToLines(String fileName) {
+        ArrayList<String> liste = new ArrayList<>();
+
+        Path path = Paths.get(fileName);
+        try (Stream<String> lines = Files.lines(path)) {
+            lines.forEach(liste::add);
+        }  catch (IOException ex) {
+            System.out.println("Can not read file: " + fileName);
+        }
+        return liste;
+    }
 
     private void readFile(String src, String dst) {
-        try (FileInputStream inputStream = new FileInputStream(src)) {
-            String srcText = IOUtils.toString(inputStream);
-            this.srcText = srcText;
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        try (FileInputStream inputStream = new FileInputStream(src)) {
+//            String srcList = IOUtils.toString(inputStream);
+//            this.srcList = srcList;
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        try (FileInputStream inputStream = new FileInputStream(dst)) {
+//            String dstList = IOUtils.toString(inputStream);
+//            this.dstList = dstList;
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+        srcList = fileToLines(src);
+        dstList = fileToLines(dst);
 
-        try (FileInputStream inputStream = new FileInputStream(dst)) {
-            String dstText = IOUtils.toString(inputStream);
-            this.dstText = dstText;
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
 
@@ -44,34 +62,66 @@ public class DmpMain {
 
         diff_match_patch dmp = new diff_match_patch();
 
-        LinkedList<diff_match_patch.Diff> dpmListe = dmp.diff_main(dmpMain.srcText, dmpMain.dstText);
+        System.out.println(dmpMain.srcList.get(1));
 
-        if (cleanup == 1) {
-            dmp.diff_cleanupSemantic(dpmListe);
-        } else if (cleanup == 2) {
-            dmp.diff_cleanupEfficiency(dpmListe);
-        } else {
-            return null;
-        }
 
-        ArrayList<diff_match_patch.Diff> dmpBufferList = new ArrayList<>(dpmListe);
+        LinkedList<diff_match_patch.Diff> dpmListe;
+
+
+
+//        int diffListSize;
+//        if (dmpMain.srcList.size() < dmpMain.dstList.size()) {
+//            diffListSize = dmpMain.dstList.size();
+//        } else {
+//            diffListSize = dmpMain.srcList.size();
+//        }
+
         ArrayList<DiffInfo> diffList = new ArrayList<>();
 
-        int diffListSize = dmpBufferList.size();
-        System.out.println(diffListSize);
+        LinkedList<diff_match_patch.Diff> fullDiff = dmp.diff_main(String.join("\n", dmpMain.srcList), String.join("\n", dmpMain.dstList));
 
+        switch (cleanup) {
+            case 1:
+                dmp.diff_cleanupSemantic(fullDiff);
+                break;
+            case 2:
+                dmp.diff_cleanupEfficiency(fullDiff);
+                break;
+            default:
+                System.out.println("No cleanup. Raw Data!");
+                break;
+        }
 
-        for (int i = 0; i <= diffListSize; i++) {
+        int fullDiffSize = fullDiff.size();
+
+        for (int i = 0; i <= fullDiffSize; i++) {
             DiffInfo diffInfo = new DiffInfo();
 
-            diff_match_patch.Operation getOperation = dmpBufferList.get(i).operation;
-            if (getOperation.equals("EQUAL")) {
-                continue;
-            } else if (getOperation.equals("INSERT")) {
-                diffInfo.setActionType("INSERT");
-            } else if (getOperation.equals("DELETE")) {
-                diffInfo.setActionType("DELETE");
+            dpmListe = dmp.diff_main(dmpMain.srcList.get(i), dmpMain.dstList.get(i));
+
+            // Is cleanup Parameter set and what type
+
+
+            // Convert LinkedList(dmpListe) to ArrayList
+            ArrayList<diff_match_patch.Diff> dmpArrayList = new ArrayList<>(dpmListe);
+
+            for (diff_match_patch.Diff diff : dmpArrayList) {
+                switch (diff.operation) {
+                    case EQUAL:
+                        break;
+                    case DELETE:
+                        diffInfo.setActionType("DELETE");
+                        break;
+                    case INSERT:
+                        diffInfo.setActionType("INSERT");
+                        break;
+                    default:
+                        System.out.println("Failed to setActionType: DmpMain, Line ca. 103-113");
+                }
+
             }
+
+            diffInfo.setSrcID(i);
 
             diffList.add(diffInfo);
         }
@@ -79,4 +129,6 @@ public class DmpMain {
 
         return diffList;
     }
+
+
 }
